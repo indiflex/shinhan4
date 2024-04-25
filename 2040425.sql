420216a0191d7216464eb25c0722f0410bad5c98
indiflex <indiflex1@gmail.com>
2024-04-25 09:45:36 +0900

    Emp salary update

---
+++ 2040425.sql
@@ -0,0 +1,11 @@
+show tables;
+
+select * from testdb.Emp;
+
+select * from Emp 
+-- update Emp set salary = 220
+ where id = 3;
+ 
+show processlist;
+kill 25; -- kill query
+
---
+++ salary_update.sql
@@ -0,0 +1,18 @@
+start transaction;
+
+select * from Emp;
+
+update Emp set salary = 230
+ where id = 3;
+
+update Emp set salary = 240
+ where id = 4;
+
+-- rollback;
+
+-- commit;
+
+
+
+
+
